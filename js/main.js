1b678ba2b0afa75d836727f4a362baf6e759ce4d
Diego Augusto <contactdiegonow@gmail.com>
2020-01-07 08:50:47 -0300

    fixes para funcionamento dinamico

--- js/main.js
+++ js/main.js
@@ -37,23 +37,23 @@ function setBgGreet() {
         hour = today.getHours(),
         min = today.getMinutes();
 
-    if (hour < 5) {
+    if (hour <= 5) {
         //madrugada
-        document.body.style.backgroundImage = "url('../imgs/dawn.gif')";
-    } else if (hour < 7) {
+        document.body.style.backgroundImage = "url('../imgs/dawn.gif')"
+    } else if (hour <= 7) {
+        //amanhecendo
+        document.body.style.backgroundImage = "url('../imgs/dawning.gif')";
+    } else if (hour <= 9) {
         //manhãzinha
-        document.body.style = "color: white; transition: ";
+        document.body.style = "color: white;";
         document.getElementById('h1').style = "text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #e60073, 0 0 40px #e60073, 0 0 50px #e60073, 0 0 60px #e60073, 0 0 70px #e60073;";
         document.getElementById('h2').style = "text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #4d68ff, 0 0 40px #4d68ff, 0 0 50px #4d68ff, 0 0 60px #4d68ff, 0 0 70px #4d68ff; opacity: 1;";
         document.getElementById('focus').style = "text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #4d68ff, 0 0 40px #4d68ff, 0 0 50px #4d68ff, 0 0 60px #4d68ff, 0 0 70px #4d68ff; opacity: 1;";
         document.getElementById('time').style = "text-shadow: 0 0 20px #fff, 0 0 30px #4dffa6, 0 0 40px #4dffa6, 0 0 50px #4dffa6, 0 0 60px #4dffa6, 0 0 70px #4dffa6, 0 0 80px #4dffa6;";
         document.body.style.backgroundImage = "url('../imgs/day.gif')";
-
-    } else if (hour < 8) {
-        //amanhecendo
-        document.body.style.backgroundImage = "url('../imgs/afternoon.gif')";
     } else {
         //noite
+        document.body.style.backgroundImage = "url('../imgs/anoitecendo.gif')";
     }
 }
 
